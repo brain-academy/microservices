7a880c43356f7bc8aff6d290aec5815c698955d1
David Urvoy <daviid.urvoy@gmail.com>
2021-04-25 20:33:06 +0000

    feat: notify user when maximum pings reached

--- event-service/event-spi/event-discord-client/src/discord-client.ts
+++ event-service/event-spi/event-discord-client/src/discord-client.ts
@@ -1,18 +1,40 @@
+import Discord, {MessageReaction, TextChannel} from 'discord.js'
 import {EventPort} from 'event-domain'
-import Discord, {Intents} from 'discord.js'
 import config from '../config.json'
 
-const intents = new Intents([
-    "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", // lets you request guild members (i.e. fixes the issue)
-])
-const client = new Discord.Client({ws: {intents}})
+const PLAYERS_REGEX = /.*joueurs maximum.*/
+const MAXIMUM_PLAYERS_REGEX = /(\d+)[^,.\d\n]+?(?=(joueurs maximum))/
+
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 client.login(config.token)
+
+client.once('ready', () => {
+    console.log('discord-client is ready...')
+    return client.channels.fetch(config.botc_irl_channel)
+        .then(channel => (<TextChannel>channel).messages.fetch({limit: 10}))
+})
+client.on('messageReactionAdd', ({message, emoji: {name}}, user) => {
+    if(name === '✅') {
+        console.log({message: message.embeds})
+        const maxPlayersCount = getMaxPlayers(message.embeds[0].description || '')
+        let reactions = message.reactions.cache.array();
+        (reactions
+            .find(reaction => reaction.emoji.name === '✅') as MessageReaction).users
+            .fetch()
+            .then(users => {
+                if(users.size - 1 > maxPlayersCount)
+                    user.send(`Hello ${user.username} !\nDésolé, mais le nombre maximal de ${maxPlayersCount} participants prévu pour cet événement est déjà atteint... :sob:\nTu es néanmoins placé sur la liste d'attente et passera en liste principale si une place venait à se libérer. :fingers_crossed: Tu peux éventuellement contacter l'organisateur de l'événement pour savoir s'il peut te faire une place.`)
+            })
+    }
+})
+
+const getMaxPlayers: (message: string) => number = (message: string) => +(message.match(PLAYERS_REGEX)?.[0].match(MAXIMUM_PLAYERS_REGEX)?.[0] || Number.MAX_VALUE)
 
 export class DiscordClient implements EventPort {
 
     postInvite(message: string): void {
-    client.channels.fetch(config.botc_irl_channel)
-        .then(channel => (<Discord.TextChannel>channel).send(message))
+        client.channels.fetch(config.botc_irl_channel)
+            .then(channel => (<Discord.TextChannel>channel).send(message))
     }
 
 }
